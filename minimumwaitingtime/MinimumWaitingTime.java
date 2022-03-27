844d4077ab71c7d90a3c543d747aa3c4adc2e1e5
Bruno Macedo <brunooliveira.mac@gmail.com>
2022-03-27 12:07:33 -0300

    Minimum Waiting Time

--- minimumwaitingtime/MinimumWaitingTime.java
+++ minimumwaitingtime/MinimumWaitingTime.java
@@ -1,7 +1,48 @@
+import java.util.Arrays;
+
 public class MinimumWaitingTime {
 
     public int minimumWaitingTime(int[] queries) {
         // Write your code here.
-        return -1;
+        //0 + 3 + 5 + 6 + 8
+        // Sorting
+        // 1, 2, 2, 3, 6
+        // 0 + 1 + 3 + 5 + 8
+        //
+
+        Arrays.sort(queries);
+        // 1, 2, 2, 3, 6
+        int sum = 0;
+        int[] reverse = new int[queries.length];
+
+        for (int i = 0; i < queries.length; i++) {
+            reverse[i] = queries[i];
+            if (i == 0) {
+                continue;
+            }
+            for (int j = 0; j < reverse.length; j++) {
+                if (j == i){
+                    continue;
+                }
+                sum += reverse[j];
+            }
+        }
+        return sum;
     }
+
+    public static void main(String[] args) {
+        MinimumWaitingTime minimumWaitingTime = new MinimumWaitingTime();
+
+        int[] arr = new int[5];
+        arr[0] = 3;
+        arr[1] = 2;
+        arr[2] = 1;
+        arr[3] = 2;
+        arr[4] = 6;
+
+        System.out.println(minimumWaitingTime.minimumWaitingTime(arr));
+
+    }
+
 }
+
