b9b0ce76820855f71a40879d77fdad927b25f515
Bruno Macedo <brunooliveira.mac@gmail.com>
2022-04-01 00:23:13 -0300

    RemoveDuplicatesFromLinkedList

---
+++ removeduplicatesfromlinkedlist/LinkedList.py
@@ -0,0 +1,16 @@
+class LinkedList:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+def removeDuplicatesFromLinkedList(linkedList):
+    # Write your code here.
+    return None
+
+
+if __name__ == '__main__':
+
+    LinkedList(2)
+
+    removeDuplicatesFromLinkedList()
