037b8fc94428d2c27bb1dbee59123a4e080f8daa
Bruno Macedo <brunooliveira.mac@gmail.com>
2022-04-09 23:40:16 -0300

    BinarySearch

--- binarysearch/BinarySearch.py
+++ binarysearch/BinarySearch.py
@@ -1,12 +1,32 @@
-def binarySearch(array, target):
-    middle = (0 + len(array) - 1)/2   # -1 pq quer a ultima posição
-    potentialMatch = array[middle]
+# def binarySearch(array, target, leftPointer, rightPointer):
+#     middle = (left_pointer + right_pointer) // 2  # -1 pq quer a ultima "posição"
+#     potentialMatch = array[middle]
+#
+#     if (potentialMatch == target):
+#         retorna a posição dele
+#         return middle
+#     if (potentialMatch > target):
+#         middle = (left_pointer + (middle - 1)) // 2
+#     if (potentialMatch < target):
+#         middle = ((middle + 1) + rightPointer) // 2
+#
+#     pass
 
-    if (array[potentialMatch] == target):
-        return potentialMatch
-    if (array[potentialMatch] > target):
-        middle = ((middle + 1) + len(array) - 1) / 2
-    if (array[potentialMatch] < target):
-        middle = (0 + (middle + 1)) / 2
+def binarySearch(arr, target, left_pointer, right_pointer):
+    middle = (left_pointer + right_pointer) // 2  # -1 pq quer a ultima "posição"
+    potentialMatch = arr[middle]
 
-    pass
+    if potentialMatch == target:
+        return middle
+    if potentialMatch > target:
+        return binarySearch(arr, target, left_pointer, middle - 1)
+    if potentialMatch < target:
+        return binarySearch(arr, target, middle + 1, right_pointer)
+
+    return -1
+
+
+if __name__ == '__main__':
+    array = [0, 1, 21, 33, 45, 45, 61, 71, 72, 73]
+    result = binarySearch(array, 33, 0, len(array) - 1)
+    print(result)
