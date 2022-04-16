79684390161c0d4d2c1340ae2a5b511a09bcebe6
Bruno Macedo <brunooliveira.mac@gmail.com>
2022-04-16 00:59:10 -0300

    Gredy

--- fractionalknapsack/FractionalKnapsack.java
+++ fractionalknapsack/FractionalKnapsack.java
@@ -1,11 +1,73 @@
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class FractionalKnapsack {
-    private static double getOptimalValue(int capacity, int[] values, int[] weights) {
-        double value = 0;
-        //write your code here
 
-        return value;
+    static class ItemValue {
+        Double cost;
+        double wt, val, ind;
+
+        // item value function
+        public ItemValue(int wt, int val, int ind)
+        {
+            this.wt = wt;
+            this.val = val;
+            this.ind = ind;
+            cost = new Double((double)val / (double)wt);
+        }
+    }
+
+
+    private static double getOptimalValue(int[] wt, int[] val,
+                                      int capacity)
+    {
+        ItemValue[] iVal = new ItemValue[wt.length];
+
+        for (int i = 0; i < wt.length; i++) {
+            iVal[i] = new ItemValue(wt[i], val[i], i);
+        }
+
+        // sorting items by value;
+        Arrays.sort(iVal, new Comparator<ItemValue>() {
+            @Override
+            public int compare(ItemValue o1, ItemValue o2)
+            {
+                return o2.cost.compareTo(o1.cost);
+            }
+        });
+
+        double totalValue = 0d;
+
+        for (ItemValue i : iVal) {
+
+            int curWt = (int)i.wt;
+            int curVal = (int)i.val;
+
+            if (capacity - curWt >= 0) {
+                // this weight can be picked while
+                capacity = capacity - curWt;
+                totalValue += curVal;
+            }
+            else {
+                // item cant be picked whole
+                double fraction
+                        = ((double)capacity / (double)curWt);
+                totalValue += (curVal * fraction);
+                capacity
+                        = (int)(capacity - (curWt * fraction));
+                break;
+            }
+        }
+
+        return totalValue;
     }
 
     public static void main(String args[]) {
@@ -18,6 +80,9 @@ public class FractionalKnapsack {
             values[i] = scanner.nextInt();
             weights[i] = scanner.nextInt();
         }
-        System.out.println(getOptimalValue(capacity, values, weights));
+        double optimalValue = getOptimalValue(weights, values, capacity);
+
+
+        System.out.println(String.format(Locale.US, "%.4f", optimalValue));
     }
 }
