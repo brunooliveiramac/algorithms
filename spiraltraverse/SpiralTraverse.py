2ab188eba6d3447aba1708df6f5bf7dff2afa0ca
Bruno Macedo <brunooliveira.mac@gmail.com>
2022-05-01 16:30:35 -0300

    First Part - Spiral Traverse

--- spiraltraverse/SpiralTraverse.py
+++ spiraltraverse/SpiralTraverse.py
@@ -1,7 +1,27 @@
 def spiralTraverse(array):
-    
+    startRow, endRow = 0, len(array) - 1
+    startCol, endCol = 0, len(array[0]) - 1
+    toReturn = []
 
-    pass
+    while startRow <= endRow and startCol <= endCol:
+        for col in range(startCol, endCol + 1):
+            toReturn.append(array[startRow][col])
+
+        for row in range(startRow + 1, endRow + 1):
+            toReturn.append(array[row][endCol])
+
+        for col in reversed(range(startCol, endCol)):
+            toReturn.append(array[endRow][col])
+
+        for row in reversed(range(startRow + 1, endRow)):
+            toReturn.append(array[row][startCol])
+
+        startRow += 1
+        startCol += 1
+        endCol -= 1
+        endRow -= 1
+
+    return toReturn
 
 
 if __name__ == '__main__':
