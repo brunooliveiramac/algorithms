953b72aae17d126e77024769a3b2a407de697c7e
Bruno Macedo <brunooliveira.mac@gmail.com>
2022-04-30 23:42:15 -0300

    SpiralTraverse

---
+++ spiraltraverse/SpiralTraverse.py
@@ -0,0 +1,15 @@
+def spiralTraverse(array):
+    
+
+    pass
+
+
+if __name__ == '__main__':
+    array = [
+        [1, 2, 3, 4],
+        [12, 13, 14, 5],
+        [11, 16, 15, 6],
+        [10, 9, 8, 7]
+    ]
+
+    print(spiralTraverse(array))
