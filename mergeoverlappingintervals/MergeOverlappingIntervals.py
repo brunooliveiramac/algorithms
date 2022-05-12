118c088a3224b282ac0d0a55209dffa6d4cd849e
Bruno Macedo <brunooliveira.mac@gmail.com>
2022-05-12 19:54:49 -0300

    OverlappingIntervals

---
+++ mergeoverlappingintervals/MergeOverlappingIntervals.py
@@ -0,0 +1,8 @@
+def mergeOverlappingIntervals(intervals):
+    # Write your code here.
+    return [[]]
+
+
+if __name__ == '__main__':
+    intervals = [[1, 2], [3, 5], [4, 7], [6, 8], [9, 10]]
+    print(mergeOverlappingIntervals(intervals))
