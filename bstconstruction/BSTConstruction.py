e37a1fe2f9162f02e88d6d37be12a7043d9ea8d2
Bruno Macedo <brunooliveira.mac@gmail.com>
2022-05-16 20:03:33 -0300

    Add function for BST

--- bstconstruction/BSTConstruction.py
+++ bstconstruction/BSTConstruction.py
@@ -2,6 +2,22 @@
 # the insert, contains, and remove methods.
 # Feel free to add new properties and methods
 # to the class.
+def insertNode(self, value):
+    if self is None:
+        return
+
+    if value >= self.value:
+        if self.right is None:
+            self.right = BST(value)
+            return
+        insertNode(self.right, value)
+    else:
+        if self.left is None:
+            self.left = BST(value)
+            return
+        insertNode(self.left, value)
+
+
 class BST:
     def __init__(self, value):
         self.value = value
@@ -9,8 +25,7 @@ class BST:
         self.right = None
 
     def insert(self, value):
-        # Write your code here.
-        # Do not edit the return statement of this method.
+        insertNode(self, value)
         return self
 
     def contains(self, value):
@@ -21,3 +36,37 @@ class BST:
         # Write your code here.
         # Do not edit the return statement of this method.
         return self
+
+
+if __name__ == '__main__':
+    a = BST(10)
+    b = BST(5)
+    c = BST(15)
+
+    d = BST(2)
+    e = BST(5)
+
+    f = BST(13)
+    g = BST(22)
+
+    h = BST(14)
+    i = BST(1)
+
+    a.left = b
+    a.right = c
+
+    b.left = d
+    b.right = e
+
+    d.left = i
+
+    c.left = f
+    c.right = g
+
+    f.right = h
+
+    a.insert(12)
+
+    print(f.left.value)
+    print(f.right.value)
+
