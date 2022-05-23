2db3037bb6ddf8899373da2a80ef5bbb8422177a
Bruno Macedo <brunooliveira.mac@gmail.com>
2022-05-23 20:48:34 -0300

    ValidateBST

---
+++ validateabst/ValidateBST.py
@@ -0,0 +1,15 @@
+# This is an input class. Do not edit.
+class BST:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+def validateBst(tree):
+
+    pass
+
+
+if __name__ == '__main__':
+    pass
