04876c4f458a855b1e562b185ff78cc3a63d5122
Bruno Macedo <brunooliveira.mac@gmail.com>
2022-05-24 01:03:30 -0300

    ValidateBST

--- validateabst/ValidateBST.py
+++ validateabst/ValidateBST.py
@@ -6,10 +6,32 @@ class BST:
         self.right = None
 
 
+def validateBstHelper(tree, bools):
+    if tree.right is not None:
+        if tree.value > tree.right.value:
+            bools.append(False)
+        validateBstHelper(tree.right, bools)
+    if tree.left is not None:
+        if tree.value < tree.left.value:
+            bools.append(False)
+        validateBstHelper(tree.left, bools)
+    bools.append(True)
+
+
 def validateBst(tree):
-
-    pass
-
+    bools = []
+    validateBstHelper(tree, bools)
+    return False not in bools
 
 if __name__ == '__main__':
+    a = BST(10)
+    b = BST(5)
+    c = BST(15)
+    d = BST(12)
+
+    a.right = c
+    a.left = b
+    b.right = d
+
+    print(validateBst(a))
     pass
