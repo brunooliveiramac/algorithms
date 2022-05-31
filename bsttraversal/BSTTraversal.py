1ddd518064055dbe2146c04f3d2e5dac4dcc72e1
Bruno Macedo <brunooliveira.mac@gmail.com>
2022-05-31 00:23:26 -0300

    BST Traversal

--- bsttraversal/BSTTraversal.py
+++ bsttraversal/BSTTraversal.py
@@ -1,17 +1,64 @@
+class BST:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
 def inOrderTraverse(tree, array):
-    # Write your code here.
+    if tree.left is not None:
+        inOrderTraverse(tree.left, array)
+
+    array.append(tree.value)
+
+    if tree.right is not None:
+        inOrderTraverse(tree.right, array)
     pass
 
 
 def preOrderTraverse(tree, array):
-    # Write your code here.
+    array.append(tree.value)
+
+    if tree.left is not None:
+        preOrderTraverse(tree.left, array)
+    if tree.right is not None:
+        inOrderTraverse(tree.right, array)
+
     pass
 
 
 def postOrderTraverse(tree, array):
-    # Write your code here.
+    if tree.left is not None:
+        postOrderTraverse(tree.left, array)
+    if tree.right is not None:
+        postOrderTraverse(tree.right, array)
+
+    if tree is not None:
+        array.append(tree.value)
     pass
 
 
 if __name__ == '__main__':
-    pass
+    a = BST(10)
+    b = BST(5)
+    c = BST(15)
+    d = BST(2)
+    e = BST(5)
+    f = BST(1)
+    g = BST(15)
+    h = BST(22)
+
+    a.left = b
+    a.right = c
+
+    c.right = h
+
+    b.left = d
+    b.right = e
+
+    d.left = f
+
+    arr = []
+    inOrderTraverse(a, arr)
+
+    print(arr)
