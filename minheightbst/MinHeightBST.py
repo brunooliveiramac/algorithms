544e63fa11e753f7457ac59e2a426b275172fa5a
Bruno Macedo <brunooliveira.mac@gmail.com>
2022-06-05 20:44:57 -0300

    minHeight BST

--- minheightbst/MinHeightBST.py
+++ minheightbst/MinHeightBST.py
@@ -1,5 +1,26 @@
 def minHeightBst(array):
-    pass
+    left = 0
+    right = len(array) - 1
+    middle = (left + right) // 2
+    bst = BST(array[middle])
+
+    minHeightBstHelper(left, middle, array, bst, None)
+
+    minHeightBstHelper(middle, right, array, bst, None)
+    return bst
+
+def minHeightBstHelper(left, right, array, bst, last_mid):
+
+    middle = (left + right) // 2
+
+    if last_mid is not None and middle == last_mid:
+        return
+
+    bst.insert(array[middle])
+
+    minHeightBstHelper(left, middle, array, bst, middle)
+
+    minHeightBstHelper(middle, right, array, bst, middle)
 
 
 class BST:
@@ -20,3 +41,6 @@ class BST:
             else:
                 self.right.insert(value)
 
+
+if __name__ == '__main__':
+    print(minHeightBst([1, 2, 5, 7, 10, 13, 14, 15, 22]))
