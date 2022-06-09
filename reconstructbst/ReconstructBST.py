6e46c012c49242b3efd5514891a48b11186cfd90
Bruno Macedo <brunooliveira.mac@gmail.com>
2022-06-08 23:47:35 -0300

    Reconstruct BST

---
+++ reconstructbst/ReconstructBST.py
@@ -0,0 +1,14 @@
+class BST:
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+
+def reconstructBst(preOrderTraversalValues):
+    # Write your code here.
+    return None
+
+
+if __name__ == '__main__':
+    print(reconstructBst([10, 4, 2, 1, 5, 17, 19, 18]))
