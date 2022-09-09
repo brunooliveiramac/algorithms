4ec685233b0dbf157bdffcea1f6bdac8d14887f3
Bruno Macedo <brunooliveira.mac@gmail.com>
2022-09-09 00:13:55 -0300

    First golang algorithm

--- heightbalancedbinarytree/HeightBalancedBinaryTree.go
+++ heightbalancedbinarytree/HeightBalancedBinaryTree.go
@@ -8,15 +8,65 @@ type BinaryTree struct {
 }
 
 func HeightBalancedBinaryTree(tree *BinaryTree) bool {
-	// Write your code here.
-	return false
+	if tree == nil {
+		return false
+	}
+
+	if tree.Left != nil && tree.Right != nil {
+		result := tree.Right.Value - tree.Left.Value
+		if result != 1 {
+			return false
+		}
+	}
+	HeightBalancedBinaryTree(tree.Left)
+	HeightBalancedBinaryTree(tree.Right)
+
+	return true
 }
 
 func main() {
-	root := BinaryTree{
-		Value: 1,
+
+	g := BinaryTree{
+		Value: 8,
 		Left:  nil,
 		Right: nil,
 	}
+	f := BinaryTree{
+		Value: 7,
+		Left:  nil,
+		Right: nil,
+	}
+	e := BinaryTree{
+		Value: 6,
+		Left:  nil,
+		Right: nil,
+	}
+	d := BinaryTree{
+		Value: 5,
+		Left:  &f,
+		Right: &g,
+	}
+	c := BinaryTree{
+		Value: 4,
+		Left:  nil,
+		Right: nil,
+	}
+	b := BinaryTree{
+		Value: 3,
+		Left:  nil,
+		Right: &e,
+	}
+	a := BinaryTree{
+		Value: 2,
+		Left:  &c,
+		Right: &d,
+	}
+	root := BinaryTree{
+		Value: 1,
+		Left:  &a,
+		Right: &b,
+	}
+
+
 	print(HeightBalancedBinaryTree(&root))
 }
