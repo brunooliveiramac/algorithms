7f072f270968d5f16f2b72bda15f2bd2c6036f0c
Bruno Macedo <brunooliveira.mac@gmail.com>
2022-10-01 21:20:11 -0300

    MaxSubserSumNoAdjacent

--- maxsubsetsumnoadjacent/MaxSubsetSumNoAdjacent.py
+++ maxsubsetsumnoadjacent/MaxSubsetSumNoAdjacent.py
@@ -1,5 +1,16 @@
 def maxSubsetSumNoAdjacent(array):
+    aux_arr = []
+
+    for index in range(len(array)):
+
+        if index >= 2:
+            max_at_current_position = max(array[index - 1], array[index - 2] + array[index])
+            aux_arr.append(max_at_current_position)
+        else:
+            aux_arr.append(array[index])
+    print(aux_arr)
+    return max(aux_arr)
 
 
-
-    pass
+if __name__ == '__main__':
+    print(maxSubsetSumNoAdjacent([7, 10, 12, 7, 9, 14]))
