3fc2e09c45b089243b41e171d0c1020cb8d13d6d
Bruno Macedo <brunooliveira.mac@gmail.com>
2022-10-05 01:06:13 -0300

    NumberOfWaysToMakeChange

--- numberofwaystomakeachange/NumberOfWaysToMakeAChange.py
+++ numberofwaystomakeachange/NumberOfWaysToMakeAChange.py
@@ -1,6 +1,13 @@
 def numberOfWaysToMakeChange(n, denoms):
-    # Write your code here.
-    pass
+    ways = []
+
+    for i in range(n):
+        ways.append(i)
+
+    for i in range(len(denoms)):
+        if denoms[i] <= ways[i]:
+            ways[i] = ways[i] + ways[i - 1]
+    return ways
 
 
 if __name__ == '__main__':
