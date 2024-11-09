bf9afd5f6687221334c45c4aa46540b7069a9657
Bruno Macedo <brunooliveira.mac@gmail.com>
2024-11-09 13:18:41 -0300

    two sum

---
+++ twonumbersum-go/main.go
@@ -0,0 +1,40 @@
+package main
+
+//Two Number Sum
+//
+//Write a function that takes in a non-empty array of distinct integers and an integer representing a target sum. If any two numbers in the input array sum up to the target sum, the function should return them in an array, in any order. If no two numbers sum up to the target sum, the function should return an empty array.
+//
+//Note that the target sum has to be obtained by summing two different integers in the array; you can't add a single integer to itself in order to obtain the target sum.
+//
+//You can assume that there will be at most one pair of numbers summing up to the target sum.
+//
+//Sample Input
+//
+//makefile
+//Copy code
+//array = [3, 5, -4, 8, 11, 1, -1, 6]
+//targetSum = 10
+//Sample Output
+//[-1, 11] // the numbers could be in reverse order
+
+// O(n²)
+func TwoNumberSum(array []int, target int) []int {
+	sum := make([]int, 0)
+out:
+	for i := 0; i < len(array); i++ {
+		for j := 0; j < len(array); j++ {
+			if i != j {
+				if array[i]+array[j] == target {
+					sum = append(sum, array[i])
+					sum = append(sum, array[j])
+					break out
+				}
+			}
+		}
+	}
+	return sum
+}
+
+func main() {
+	print(TwoNumberSum([]int{3, 5, -4, 8, 11, 1, -1, 6}, 10))
+}
