8b384c61202f13bc8e86c02e707bb68128d5c5fb
Bruno Macedo <brunooliveira.mac@gmail.com>
2025-10-09 01:49:31 -0300

    encode decode

--- encodedecode/encode-decode.go
+++ encodedecode/encode-decode.go
@@ -9,6 +9,12 @@ func NewSolution() Solution {
 }
 
 func (s *Solution) Encode(strs []string) string {
+	if len(strs) == 1 && strs[0] == "" {
+		return ""
+	}
+	if len(strs) == 0 {
+		return "nil"
+	}
 	result := ""
 
 	for _, n := range strs {
@@ -23,10 +29,37 @@ func (s *Solution) Encode(strs []string) string {
 }
 
 func (s *Solution) Decode(st string) []string {
+	if st == "" {
+		return []string{""}
+	}
+	if st == "nil" {
+		return []string{}
+	}
 	var arr []string
+	size := 0
+	incrementSize := 0
+	stringToBuild := ""
 
-	for _, str := range st {
-		slen := str
+	for i, str := range st {
+		if str == '#' {
+			u := string(st[i-1])
+			size, _ = strconv.Atoi(u)
+		}
+		if size != 0 && string(str) != "#" {
+			if size != incrementSize {
+				stringToBuild += string(str)
+				incrementSize += 1
+				if size == incrementSize && len(st) == i+1 {
+					arr = append(arr, stringToBuild)
+					println(stringToBuild)
+				}
+			} else {
+				incrementSize = 0
+				arr = append(arr, stringToBuild)
+				println(stringToBuild)
+				stringToBuild = ""
+			}
+		}
 	}
 
 	return arr
@@ -35,7 +68,7 @@ func (s *Solution) Decode(st string) []string {
 func main() {
 
 	solution := NewSolution()
-	arr := []string{"neet", "code", "love", "you"}
+	arr := []string{""}
 
 	encode := solution.Encode(arr)
 	solution.Decode(encode)
